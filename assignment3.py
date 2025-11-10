587079b21b14b82a9754bba7b14c44f17ac6e8e3
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2025-11-09 20:22:01 -0500

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,50 @@
+import random
+
+def new_code(filename, duplicates, n):
+    new_colors = []
+    
+    # Read file lines into list
+    with open(filename) as f:
+        file = f.read()
+    
+    for color in file:
+        new_colors.append(color)
+
+    # not enough colors and duplicates not allowed
+    if not duplicates and n > len(file):
+        return False
+
+    # randomly remove until list length == n
+    while len(new_colors) > n:
+        delete = random.randint(0, len(new_colors) - 1)
+        new_colors.pop(delete)
+    
+    # Handle duplicates if not allowed
+    if not duplicates:
+        unique_colors = []
+        for color in new_colors:
+            if color not in unique_colors:
+                unique_colors.append(color)
+            else:
+                # replace with a random unique color
+                replacement = random.choice(file)
+                # keeps finding new replacement on the off chance that the replacement is the same color
+                while replacement in unique_colors:
+                    replacement = random.choice(file)
+                unique_colors.append(replacement)
+        new_colors = unique_colors
+
+    # if list shorter than n (can happen if too many duplicates removed)
+    while len(new_colors) < n:
+        choice = random.choice(file)
+        if duplicates or choice not in new_colors:
+            new_colors.append(choice)
+
+    return new_colors
+
+
+def score_red(code, guess):
+    ...
+    
+def score_white(code, guess, redlist):
+    ...
